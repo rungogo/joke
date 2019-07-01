409e8d1e4042b778990d7139b44b0a40ad06f02a
lad <liuandong@keking.cn>
2019-07-01 13:38:38 +0800

    新增角色实体类

---
+++ src/main/java/com/lad/chestnut/pojo/model/Role.java
@@ -0,0 +1,32 @@
+package com.lad.chestnut.pojo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author lad
+ * 角色表
+ */
+@Entity
+@Table(name = "role")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    /**
+     * 角色名
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * 角色名中文名
+     */
+    @Column(name = "nameZh")
+    private String nameZh;
+}
